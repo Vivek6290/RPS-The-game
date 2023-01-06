e33550790ed24c663d4afc00699e2623d00d35f5
Vivek6290 <vg629066@gmail.com>
2023-01-06 22:20:54 +0530

    Added script for Modals.

---
+++ scripts/index.js
@@ -0,0 +1,46 @@
+
+// Get the button that opens the modal
+//var btn = document.getElementById("rule-button");
+
+// Get the <span> element that closes the modal
+//var span = document.getElementsByClassName("close-button")[0];
+
+// When the user clicks on the button, open the modal
+ function clickme() {
+    var modal = document.getElementById("Rules-id");
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function closeme() {
+  var modal = document.getElementById("Rules-id");
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  var modal = document.getElementById("Rules-id");
+  var modal1 = document.getElementById("About-id");
+  if (event.target == modal) {
+    modal.style.display = "none";
+    }
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+}
+function clickme1() {
+    var modal = document.getElementById("About-id");
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function closeme1() {
+  var modal = document.getElementById("About-id");
+  modal.style.display = "none";
+}
+//window.onclick = function(event) {
+//  var modal = document.getElementById("About-id");
+//  if (event.target == modal) {
+  //  modal.style.display = "none";
+//  }
+//}
